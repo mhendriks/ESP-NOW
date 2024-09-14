3757a2a3b7a2190e53b5b8cedc43a01d623e7432
mhendriks <m.a.hendriks@gmail.com>
2024-09-14 12:23:07 +0200

    init

---
+++ examples/ESP_NOW_Broadcast_Slave/version.h
@@ -0,0 +1,17 @@
+#pragma once
+
+//The version number conforms to semver.org format
+#define _VERSION_MAJOR 0
+#define _VERSION_MINOR 0
+#define _VERSION_PATCH 0
+#define _VERSION_BUILD 1
+#define _VERSION_DATE "09/09/2024"
+#define _VERSION_DATE_ISO "2024-09-09"
+#define _VERSION_TIME "23:15:33"
+#define _VERSION_ONLY "0.0.0"
+#define _VERSION_NOBUILD "0.0.0 (09/09/2024)"
+#define _VERSION_NOBUILD_ISO "0.0.0 (2024-09-09)"
+#define _VERSION "0.0.0+1 (09/09/2024)"
+#define _VERSION_ISO "0.0.0+1 (2024-09-09)"
+
+//The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
